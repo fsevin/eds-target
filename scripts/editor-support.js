1a74363b2a026cc7e0eb42f21e0c428173d809af
François Fornaciari <francois.fornaciari@gmail.com>
2025-11-17 20:29:04 +0100

    update

--- scripts/editor-support.js
+++ scripts/editor-support.js
@@ -118,6 +118,6 @@ decorateRichtext();
 const observer = new MutationObserver(() => decorateRichtext());
 observer.observe(document, { attributeFilter: ['data-richtext-prop'], subtree: true });
 
-document.querySelectorAll('[data-aue-resource]').forEach((el) => {
-  el.removeAttribute('data-aue-resource');
+document.querySelectorAll('[data-aue-filter]').forEach((el) => {
+  el.removeAttribute('data-aue-filter');
 });
