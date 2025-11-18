17cbaf734146a2ea610b9a04a5a77ae0c036d5a3
François Fornaciari <francois.fornaciari@gmail.com>
2025-11-18 05:41:36 +0100

    update

--- scripts/editor-support.js
+++ scripts/editor-support.js
@@ -127,10 +127,11 @@ if (!window.location.href.includes('/master/')) {
 
   document.querySelectorAll('[data-aue-type]').forEach((el) => {
     if (/*el.tagName.toLowerCase() === 'body' || */el.getAttribute('data-aue-type') === 'container' || el.getAttribute('data-aue-type') === 'column') {
-      el.removeAttribute('data-aue-resource');
+      //el.removeAttribute('data-aue-resource');
+      body.removeAttribute('data-aue-type');
     }
     const body = document.querySelector('body');
-    body.setAttribute('data-aue-type', 'container');
+    body.removeAttribute('data-aue-type');
   });
 }
 
