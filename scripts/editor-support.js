a1395f1e910247643bd1a9c0a55cfa4c97ae1a30
François Fornaciari <francois.fornaciari@gmail.com>
2025-11-18 05:18:11 +0100

    update

--- scripts/editor-support.js
+++ scripts/editor-support.js
@@ -120,8 +120,13 @@ observer.observe(document, { attributeFilter: ['data-richtext-prop'], subtree: t
 
 
 if (!window.location.href.includes('/master/')) {
+  const meta = document.createElement('meta');
+  meta.name = 'urn:adobe:aue:config:disable';
+  meta.content = 'duplicate,copy';
+  document.getElementsByTagName('head')[0].appendChild(meta);
+
   document.querySelectorAll('[data-aue-type]').forEach((el) => {
-    if (el.getAttribute('data-aue-type') === 'container' || el.getAttribute('data-aue-type') === 'column') {
+    if (el.name === 'body' || el.getAttribute('data-aue-type') === 'container' || el.getAttribute('data-aue-type') === 'column') {
       el.removeAttribute('data-aue-resource');
     }
   });
