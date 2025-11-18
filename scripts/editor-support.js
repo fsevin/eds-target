8f64037aa80c0ca90f09e6a133a4e59a684aca22
François Fornaciari <francois.fornaciari@gmail.com>
2025-11-18 05:26:17 +0100

    update

--- scripts/editor-support.js
+++ scripts/editor-support.js
@@ -126,8 +126,9 @@ if (!window.location.href.includes('/master/')) {
   document.getElementsByTagName('head')[0].appendChild(meta);
 
   document.querySelectorAll('[data-aue-type]').forEach((el) => {
-    console.log('Removing AUE resource attribute from', el);
+    console.log('Checking AUE element', el.tagName);
     if (el.tagName === 'body' || el.getAttribute('data-aue-type') === 'container' || el.getAttribute('data-aue-type') === 'column') {
+      console.log('Removing AUE resource attribute from', el.tagName);
       el.removeAttribute('data-aue-resource');
     }
   });
