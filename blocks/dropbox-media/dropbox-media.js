ff2776ff27902da7d662ace82ed91bbde160e8f1
François Fornaciari <francois.fornaciari@gmail.com>
2025-11-24 09:18:54 +0100

    update

--- blocks/dropbox-media/dropbox-media.js
+++ blocks/dropbox-media/dropbox-media.js
@@ -2,16 +2,19 @@ import { readBlockConfig } from '../../scripts/aem.js';
 
 export default async function decorate(block) {
   const config = readBlockConfig(block);
-
   const dropboxMedia = config.dropboxmedia;
 
-  const content = document.createRange().createContextualFragment(`
-    <img
-      src="${dropboxMedia}"
-      alt="Dropbox Media"
-      class="dropbox-media-image"
-    />
-  `);
+  if (dropboxMedia) {
+    content = document.createRange().createContextualFragment(`
+      ${dropboxMedia} ? 
+        <img
+          src="${dropboxMedia}"
+          alt="Dropbox Media"
+          class="dropbox-media-image"
+        /> :
+        <div>No Dropbox media provided.</div>
+    `);
+  }
 
   block.textContent = '';
   block.append(content);
