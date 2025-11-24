ace1267c776d6c3ab177819845eb202150208f18
François Fornaciari <francois.fornaciari@gmail.com>
2025-11-24 09:19:10 +0100

    update

--- blocks/dropbox-media/dropbox-media.js
+++ blocks/dropbox-media/dropbox-media.js
@@ -4,17 +4,15 @@ export default async function decorate(block) {
   const config = readBlockConfig(block);
   const dropboxMedia = config.dropboxmedia;
 
-  if (dropboxMedia) {
-    content = document.createRange().createContextualFragment(`
-      ${dropboxMedia} ? 
-        <img
-          src="${dropboxMedia}"
-          alt="Dropbox Media"
-          class="dropbox-media-image"
-        /> :
-        <div>No Dropbox media provided.</div>
-    `);
-  }
+  const content = document.createRange().createContextualFragment(`
+    ${dropboxMedia} ? 
+      <img
+        src="${dropboxMedia}"
+        alt="Dropbox Media"
+        class="dropbox-media-image"
+      /> :
+      <div>No Dropbox media provided.</div>
+  `);
 
   block.textContent = '';
   block.append(content);
