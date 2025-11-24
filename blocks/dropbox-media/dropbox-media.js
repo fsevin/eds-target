b2c8014a75dffc00aad42fb691ae5cc6335da5b6
François Fornaciari <francois.fornaciari@gmail.com>
2025-11-24 10:03:06 +0100

    update

--- blocks/dropbox-media/dropbox-media.js
+++ blocks/dropbox-media/dropbox-media.js
@@ -6,9 +6,10 @@ export default async function decorate(block) {
   const dropboxMedia = config.dropboxmedia || '';
   const title = config.title || 'Lorem Ipsum Dolor Sit';
   const description = config.description || 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+  const inverted = config.inverted === 'true' || config.inverted === true;
 
   const content = document.createRange().createContextualFragment(`
-    <div class="dropbox-media">
+    <div class="dropbox-media${inverted ? ' inverted' : ''}">
       <div class="dropbox-media-content">
         <h2 class="dropbox-media-title" data-aue-label="Title" data-aue-prop="title" data-aue-type="text">${title}</h2>
         <p class="dropbox-media-description" data-aue-label="Description" data-aue-prop="description" data-aue-type="text">${description}</p>
