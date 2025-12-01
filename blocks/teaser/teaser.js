617a2016bac1c9f42ce12026945a6ff8b86a07f0
François Fornaciari <francois.fornaciari@gmail.com>
2025-12-01 11:12:20 +0100

    u

--- blocks/teaser/teaser.js
+++ blocks/teaser/teaser.js
@@ -83,8 +83,8 @@ export default function decorate(block) {
     pictureHTML = createPlaceholderSVG('image', '4:3');
   }
 
-  const flipLayout = config.flipLayout === 'true' || config.flipLayout === true;
-  const showArrow = config.showArrow === 'true' || config.showArrow === true;
+  const flipLayout = config.fliplayout === 'true' || config.fliplayout === true;
+  const showArrow = config.showarrow === 'true' || config.showarrow === true;
 
   const arrowIcon = showArrow ? `<svg class="inline-block w-5 h-5 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8l4 4m0 0l-4 4m4-4H3"></path>
