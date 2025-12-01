2c99db1141570a7ee4bd78db289ac1510cdee549
François Fornaciari <francois.fornaciari@gmail.com>
2025-12-01 13:56:25 +0100

    u

--- scripts/aem.js
+++ scripts/aem.js
@@ -321,7 +321,7 @@ function createOptimizedPicture(
     if (i < breakpoints.length - 1) {
       const source = document.createElement('source');
       if (br.media) source.setAttribute('media', br.media);
-      source.setAttribute('srcset', `${pathname}?width=${br.width}&format=${ext}&optimize=medium`);
+      source.setAttribute('srcset', `${pathname}?width=${br.width}&format=${ext}&optimize=high`);
       picture.appendChild(source);
     } else {
       const img = document.createElement('img');
