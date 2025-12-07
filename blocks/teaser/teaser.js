b042e016b12bbe47f9ca4e9c750d135d8e793049
François Fornaciari <francois.fornaciari@gmail.com>
2025-12-07 15:27:47 +0100

    u

--- blocks/teaser/teaser.js
+++ blocks/teaser/teaser.js
@@ -111,7 +111,7 @@ export default async function decorate(block) {
   const content = document.createRange().createContextualFragment(`
     <section class="py-20 py-20 bg-white">
       <div class="container mx-auto px-4">
-        <div class="grid lg:grid-cols-5 gap-12 items-center">
+        <div class="grid lg:grid-cols-5 gap-12 items-center border border-gray-200 rounded-2xl p-8 bg-gray-50">
           ${flipLayout ? textBlock + imageBlock : imageBlock + textBlock}
         </div>
       </div>
